d22076c5239863e2f96f9a76d5d5ff4df2c00f5f
swanbreton <swan.breton75@gmail.com>
2023-12-04 09:50:22 +0100

    add foodById route

--- routers/foodsRouter.js
+++ routers/foodsRouter.js
@@ -4,5 +4,5 @@ const foodController = require('../controllers/foodsController');
 
 //------------- Routes -------------
 router.get('/', foodController.getFoods);
-
+router.get('/:id', foodController.getFoodById);
 module.exports = router;
