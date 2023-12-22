a4928439eaea4cc748d7c0c25e025fc2db4abcb1
Yann <yann.sady.pro@gmail.com>
2023-12-22 16:25:30 +0100

    force db : false

--- server.js
+++ server.js
@@ -7,12 +7,23 @@ const bcrypt = require('bcrypt');
 const port = 8000;
 
 //------------------------ Connected database ------------------------
-db.sequelize.sync({force: true}).then(async () => {
+db.sequelize.sync({force: false}).then(async () => {
   console.log('Database connected and synchronized');
 
-  await require('./seeders/20231222021145-roles-seed').up(db.sequelize.getQueryInterface());
-  await require('./seeders/20231222022250-admin-seed').up(db.sequelize.getQueryInterface());
-
+  const admin = await db.employees.findOne({
+    where: {
+        id: 1
+    }
+  });
+  if (!admin) {
+    await require('./seeders/20231222022250-admin-seed').up(db.sequelize.getQueryInterface());
+  }
+  
+  const roles = await db.roles.findAll();
+  if (roles.length == 0) {
+    await require('./seeders/20231222021145-roles-seed').up(db.sequelize.getQueryInterface());
+  }
+  
   app.listen(port, () => {
     console.log('Server is running on http://localhost:' + port);
   });
