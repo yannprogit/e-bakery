a7dbac666bb0f869ce1a78702e0d658b2677295c
Yann <yann.sady.pro@gmail.com>
2023-12-24 07:23:10 +0100

    fix bug controller employees

--- controllers/employeesController.js
+++ controllers/employeesController.js
@@ -1,5 +1,5 @@
 //------------- Import -------------
-const { getEmployees, getEmployeesByRole, addEmployee, deleteEmployeeById, getEmployeeById, updateEndContract } = require('../services/employeesService.js');
+const { getEmployees, getEmployeesByRole, addEmployee, deleteEmployeeById, updateEmployeeByEmployee, updateEmployeeByAdmin, updateEndContract } = require('../services/employeesService.js');
 const bcrypt = require('bcrypt');
 
 //------------- Methods -------------
