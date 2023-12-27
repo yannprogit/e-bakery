525d37a30f6e6b1ace95ce162a5733ee032556d4
Yann <yann.sady.pro@gmail.com>
2023-12-27 18:31:26 +0100

    remove if for role -> verif in yaml

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -33,7 +33,7 @@ exports.login = async (req, res) => {
                 where: {mail: req.body.mail}
             });
         }
-        else if (role=="employee") {
+        else {
             user = await db.employees.findOne({
                 where: {mail: req.body.mail}
             });
@@ -46,9 +46,6 @@ exports.login = async (req, res) => {
                     return res.status(422).json({success: false, message: 'Your contract has expired, you can no longer connect'});
                 }
             } 
-        }
-        else {
-            return res.status(400).json({success: false, message: 'The role must be customer or employee'});
         }
 
         if (user) {
